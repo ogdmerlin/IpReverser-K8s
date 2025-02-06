9c02fae9e8afd3f86685e08affdd666f30365548
ogdmerlin <ogdmerlin@gmail.com>
2025-02-06 20:25:35 +0200

    add: Database connection

--- app/db.py
+++ app/db.py
@@ -5,8 +5,7 @@ from sqlalchemy.orm import sessionmaker
 # For local dev, you can set environment variables or default to something
 # We assume you have a DB_URL env var like:
 # postgresql://username:password@db-hostname:5432/db_name
-DATABASE_URL = os.getenv(
-    "DATABASE_URL", "postgresql://user:password@db:5432/mydatabase")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True,
                        pool_size=5, max_overflow=10)
