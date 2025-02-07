e34f0f9944d675948135c155f4ed19b73b4363ef
ogdmerlin <ogdmerlin@gmail.com>
2025-02-07 03:24:39 +0200

    fix: Infra Deployment made bette

--- K8s_Infra/output.tf
+++ K8s_Infra/output.tf
@@ -2,15 +2,7 @@ output "cluster_id" {
   value = format("doctl kubernetes cluster kubeconfig save %s", digitalocean_kubernetes_cluster.my_cluster.id)
 }
 
-# output "db_password" {
-#   value = random_string.db_password.result
-# }
-
-# output "db_user" {
-#   value = digitalocean_database_user.db_user.name
-# }
-
-# output "db_hostname" {
-#   value = digitalocean_database_cluster.db_cluster.endpoint
-
-# }
+output "kubeconfig" {
+  value = digitalocean_kubernetes_cluster.my_cluster.kube_config[0].raw_config
+  sensitive = true
+}
